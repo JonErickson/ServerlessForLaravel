cc2b0f3853ab2596b3998eedf3b86ccb504f81bd
Jon Erickson <jon@deschutesdesigngroup.com>
2021-11-28 12:32:09 -0800

    Added PHP-FPM runtime

--- layers/web/bootstrap.php
+++ layers/web/bootstrap.php
@@ -1,5 +1,8 @@
 <?php declare(strict_types=1);
 
+use Bref\Runtime\LambdaRuntime;
+use Bref\Runtime\PhpFpm;
+
 // Display all errors
 ini_set('display_errors', '1');
 error_reporting(E_ALL);
@@ -10,3 +13,40 @@ $appRoot = getenv('LAMBDA_TASK_ROOT');
 // Required vendor and bootstrap file
 require $appRoot . '/vendor/autoload.php';
 require __DIR__ . '/laravelBootstrap.php';
+
+// Get lambda runtime
+$lambdaRuntime = LambdaRuntime::fromEnvironmentVariable('fpm');
+
+// Check to make sure our handler exists
+$handler = $appRoot . '/' . getenv('_HANDLER');
+if (! is_file($handler)) {
+	$lambdaRuntime->failInitialization("Handler `$handler` doesn't exist");
+}
+
+// New PHP-FPM instance
+$phpFpm = new PhpFpm($handler);
+
+// Try and start
+try {
+	$phpFpm->start();
+} catch (\Throwable $e) {
+	$lambdaRuntime->failInitialization('Error while starting PHP-FPM', $e);
+}
+
+// Handle invocation requests
+while (true) {
+	$lambdaRuntime->processNextEvent(function ($event) use ($phpFpm): array {
+		$response = $phpFpm->proxy($event);
+
+		$multiHeader = array_key_exists('multiValueHeaders', $event);
+		return $response->toApiGatewayFormat($multiHeader);
+	});
+
+	// Make sure PHP is still running, echo out the exception message if it stops
+	try {
+		$phpFpm->ensureStillRunning();
+	} catch (\Throwable $e) {
+		echo $e->getMessage();
+		exit(1);
+	}
+}
